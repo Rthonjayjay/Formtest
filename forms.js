1cee92e5c6c3dfc1604dd75755e082a39db17b5b
Rthonjayjay <adeysann@gmail.com>
2023-10-01 16:59:56 +0100

    modify css,js and html

--- forms.js
+++ forms.js
@@ -1,11 +1,29 @@
-function operation (){
-    let password = document.querySelector ("#Password").value;
-    let confirmPassword = document.querySelector ("#ConfirmPassword").value;
+// function operation (){
+//     let password = document.querySelector ("#Password").value;
+//     let confirmPassword = document.querySelector ("#ConfirmPassword").value;
 
-    if (password === confirmPassword) {
-        alert("Access Granted");
-    } else {
-        alert("Access Denied. Passwords do not match.");
-    }
-}
+//     if (password === confirmPassword) {
+//         alert("Access Granted");
+//     } else {
+//         alert("Access Denied. Passwords do not match.");
+//     }
+// }
 //)
+
+let form = document.querySelector("form");
+
+// document.addEventListener("form", function() {
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent the form from submitting
+
+        let wordpass = document.querySelector("#Wordpass").value;
+        let confirmPass = document.querySelector("#ConfirmPass").value;
+
+        if (wordpass === confirmPass) {
+            alert("Access Granted");
+        } else {
+            alert("Access Denied. Passwords do not match.");
+        }
+    });
+// });
